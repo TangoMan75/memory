599db87639d615f08cc2b11f99b95bda1fc22f87
Matthias Morin <matthias.morin@gmail.com>
2018-03-26 00:42:33 +0200

    Refactor

---
+++ README.md
@@ -0,0 +1,27 @@
+Memory
+======
+
+TangoMan memory is a simple jQuery remix of memory game.
+
+Note
+====
+
+If you find any bug please report here : [Issues](https://github.com/TangoMan75/memory/issues/new)
+
+License
+=======
+
+Copyrights (c) 2018 Matthias Morin
+
+[![License][license-MIT]][license-url]
+Distributed under the MIT license.
+
+If you like **TangoMan memory** please star!
+And follow me on GitHub: [TangoMan75](https://github.com/TangoMan75)
+... And check my other cool projects.
+
+[Matthias Morin | LinkedIn](https://www.linkedin.com/in/morinmatthias)
+
+[license-GPL]: https://img.shields.io/badge/Licence-GPLv3.0-green.svg
+[license-MIT]: https://img.shields.io/badge/Licence-MIT-green.svg
+[license-url]: LICENSE
--- js/memory.js
+++ js/memory.js
@@ -1,6 +1,9 @@
-//==================================================
-// Memory
-//==================================================
+/**
+ * Memory
+ * 
+ * @author   Matthias Morin <matthias.morin@gmail.com>
+ * @version  1.0.1
+ */
 
 // Declare cards array
 var arPictures = ['camel', 'chick', 'cow', 'dog', 'elephant', 'fish', 'lamb', 'owl', 'pig', 'rabbit', 'reindeer', 'turtle'];
@@ -25,12 +28,12 @@ $('.title').on('mousedown', function () {
 function fnMakeUnselectable(objTarget) {
 
     objTarget
-        .addClass('unselectable')                      // All these attributes are inheritable
-        .attr('unselectable', 'on')                    // For IE9 - This property is not inherited, needs to be placed onto everything
-        .attr('draggable', 'false')                    // For moz and webkit, although Firefox 16 ignores this when -moz-user-select: none; is set, it's like these properties are mutually exclusive, seems to be a bug.
-        .on('dragstart', function () {
+        .addClass('unselectable')      // All these attributes are inheritable
+        .attr('unselectable', 'on')    // For IE9 - This property is not inherited, needs to be placed onto everything
+        .attr('draggable', 'false')    // For moz and webkit, although Firefox 16 ignores this when -moz-user-select: none; is set, it's like these properties are mutually exclusive, seems to be a bug.
+        .on('dragstart', function () { // Needed since Firefox 16 seems to ingore the 'draggable' attribute we just applied above when '-moz-user-select: none' is applied to the CSS
             return false;
-        }); // Needed since Firefox 16 seems to ingore the 'draggable' attribute we just applied above when '-moz-user-select: none' is applied to the CSS
+        }); 
 
     // Apply non-inheritable properties to the child elements
     objTarget
